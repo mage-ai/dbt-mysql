55c3efed6458af3f6fe9342d817826058734d13b
DANGerous <quantumventuress@gmail.com>
2023-04-16 00:52:24 -0700

    [td] Remove RuntimeException, it doesn’t exist

--- dbt/adapters/mysql/relation.py
+++ dbt/adapters/mysql/relation.py
@@ -1,7 +1,8 @@
 from dataclasses import dataclass
 
 from dbt.adapters.base.relation import BaseRelation, Policy
-from dbt.exceptions import RuntimeException
+# This class doesn’t exist.
+# from dbt.exceptions import RuntimeException
 
 
 @dataclass
@@ -26,7 +27,7 @@ class MySQLRelation(BaseRelation):
 
     def __post_init__(self):
         if self.database != self.schema and self.database:
-            raise RuntimeException(
+            raise Exception(
                 f"Cannot set `database` to '{self.database}' in mysql!"
                 "You can either unset `database`, or make it match `schema`, "
                 f"currently set to '{self.schema}'"
@@ -34,7 +35,7 @@ class MySQLRelation(BaseRelation):
 
     def render(self):
         if self.include_policy.database and self.include_policy.schema:
-            raise RuntimeException(
+            raise Exception(
                 "Got a mysql relation with schema and database set to "
                 "include, but only one can be set"
             )
